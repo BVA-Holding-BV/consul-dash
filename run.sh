c8592ab4c1471a54f5498070261934c4b00d212d
Remmelt Pit <remmelt@info.nl>
2014-12-02 18:01:04 +0100

    name

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 docker build -t consul-dash .
-docker run -e "CONSUL_LOCATION=docker02.bva.nu:8500" -d --publish 8001:80 --volume $(pwd)/app:/usr/share/nginx/html $@ consul-dash
+docker run -e "CONSUL_LOCATION=docker02.bva.nu:8500" -d --publish 8001:80 --volume $(pwd)/app:/usr/share/nginx/html $@ --name consul-dash consul-dash
