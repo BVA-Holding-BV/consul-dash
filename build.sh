0b0e24e4eb5ee6db9e43a0640c499b9f60937845
Remmelt Pit <remmelt@info.nl>
2015-01-29 15:48:30 +0100

    Typo in build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ IMAGE=bvaauctions/consul-dash
 TAG=$(git rev-parse --short HEAD)
 
 docker $DOCKER_OPTS build -t $IMAGE:$TAG .
-docker $DOCKER_OPTS push $IMAGE:latest
+docker $DOCKER_OPTS push $IMAGE:$TAG
